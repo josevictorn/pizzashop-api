cf762b0fb237df3fddf040e67028d041874be1f6
José Victor <josevictornascimento2016@gmail.com>
2025-07-26 17:10:26 -0300

    feat: add JWT and cookie support for user authentication

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,8 +1,23 @@
-import { Elysia } from 'elysia'
+import { Elysia, t } from 'elysia'
 import { registerRestaurants } from './routes/register-restaurants'
 import { sendAuthLink } from './routes/send-auth-link'
+import jwt from '@elysiajs/jwt'
+import { env } from '../env'
+import cookie from '@elysiajs/cookie'
 
-const app = new Elysia().use(registerRestaurants).use(sendAuthLink)
+const app = new Elysia()
+  .use(
+    jwt({
+      secret: env.JWT_SECRET_KEY,
+      schema: t.Object({
+        sub: t.String(),
+        restaurantId: t.Optional(t.String()),
+      }),
+    }),
+  )
+  .use(cookie())
+  .use(registerRestaurants)
+  .use(sendAuthLink)
 
 app.listen(3333, () => {
   console.log('Server is running on http://localhost:3333')
