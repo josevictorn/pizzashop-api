ea758b927be7bd04327938b89440fbd1b3ae3082
José Victor <josevictornascimento2016@gmail.com>
2025-07-26 16:42:24 -0300

    feat: implement restaurant registration endpoint

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,40 +1,7 @@
-import { Elysia, t } from 'elysia'
-import { restaurants, users } from '../db/schema'
-import { db } from '../db/connection'
+import { Elysia } from 'elysia'
+import { registerRestaurants } from './routes/register-restaurants'
 
-const app = new Elysia()
-
-app.get(
-  '/restaurants',
-  async ({ body, set }) => {
-    const { restaurantName, managerName, email, phone } = body
-
-    const [manager] = await db
-      .insert(users)
-      .values({
-        name: managerName,
-        email,
-        phone,
-        role: 'manager',
-      })
-      .returning()
-
-    await db.insert(restaurants).values({
-      name: restaurantName,
-      managerId: manager!.id,
-    })
-
-    set.status = 204
-  },
-  {
-    body: t.Object({
-      restaurantName: t.String(),
-      managerName: t.String(),
-      email: t.String({ format: 'email' }),
-      phone: t.String(),
-    }),
-  },
-)
+const app = new Elysia().use(registerRestaurants)
 
 app.listen(3333, () => {
   console.log('Server is running on http://localhost:3333')
