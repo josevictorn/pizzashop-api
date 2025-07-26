36a641451e872747d814feeb216f0be6d759ac75
José Victor <josevictornascimento2016@gmail.com>
2025-07-26 19:25:13 -0300

    feat: add authentication link handling with expiration check

---
+++ src/http/routes/authenticate-from-link.ts
@@ -0,0 +1,59 @@
+import Elysia, { t } from 'elysia'
+import { db } from '../../db/connection'
+import dayjs from 'dayjs'
+import { auth } from '../auth'
+import { authLinks } from '../../db/schema'
+import { eq } from 'drizzle-orm'
+
+export const authLinkFromCode = new Elysia().use(auth).get(
+  '/auth-links/authenticate',
+  async ({ query, jwt, setCookie, set }) => {
+    const { code, redirect } = query
+
+    const authLinkFromCode = await db.query.authLinks.findFirst({
+      where(fields, { eq }) {
+        return eq(fields.code, code)
+      },
+    })
+
+    if (!authLinkFromCode) {
+      throw new Error('Auth link not found.')
+    }
+
+    const daysSinceAuthLinkCreated = dayjs().diff(
+      authLinkFromCode.createdAt,
+      'days',
+    )
+
+    if (daysSinceAuthLinkCreated > 7) {
+      throw new Error('Auth link expired, please request a new one.')
+    }
+
+    const managedRestaurant = await db.query.restaurants.findFirst({
+      where(fields, { eq }) {
+        return eq(fields.managerId, authLinkFromCode.userId)
+      },
+    })
+
+    const token = await jwt.sign({
+      sub: authLinkFromCode.userId,
+      restaurantId: managedRestaurant?.id,
+    })
+
+    setCookie('token', token, {
+      httpOnly: true,
+      maxAge: 60 * 60 * 24 * 7, // 7 days
+      path: '/',
+    })
+
+    await db.delete(authLinks).where(eq(authLinks.code, code))
+
+    set.redirect = redirect
+  },
+  {
+    query: t.Object({
+      code: t.String(),
+      redirect: t.String(),
+    }),
+  },
+)
--- src/http/server.ts
+++ src/http/server.ts
@@ -1,23 +1,12 @@
-import { Elysia, t } from 'elysia'
+import { Elysia } from 'elysia'
 import { registerRestaurants } from './routes/register-restaurants'
 import { sendAuthLink } from './routes/send-auth-link'
-import jwt from '@elysiajs/jwt'
-import { env } from '../env'
-import cookie from '@elysiajs/cookie'
+import { authLinkFromCode } from './routes/authenticate-from-link'
 
 const app = new Elysia()
-  .use(
-    jwt({
-      secret: env.JWT_SECRET_KEY,
-      schema: t.Object({
-        sub: t.String(),
-        restaurantId: t.Optional(t.String()),
-      }),
-    }),
-  )
-  .use(cookie())
   .use(registerRestaurants)
   .use(sendAuthLink)
+  .use(authLinkFromCode)
 
 app.listen(3333, () => {
   console.log('Server is running on http://localhost:3333')
